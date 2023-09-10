802f1f22cd555606d09631a37c86abca267d83ee
Lachlan <aligoose.martin@gmail.com>
2023-09-10 16:23:21 +1000

    Update index.php

--- index.php
+++ index.php
@@ -1,3 +1,29 @@
-<?php
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Goto-Gro MRM</title>
+</head>
+<body>
+    <header>
+        <h1>Welcome to Goto-Gro MRM</h1>
+    </header>
 
-echo "Hello World! This is the beginning of the GoToGro-MRM Software Project. Test 123";
+    <nav>
+        <ul>
+            <li><a href="add_member.php">Add Member</a></li>
+            <li><a href="edit_member.php">Edit Member Details</a></li>
+            <li><a href="remove_member.php">Remove Member</a></li>
+            <li><a href="add_sales.php">Add Sales</a></li>
+            <li><a href="edit_sales.php">Edit Sales</a></li>
+            <li><a href="export_data.php">Export Sales/Members Data</a></li>
+            <li><a href="inventory.php">Inventory</a></li>
+        </ul>
+    </nav>
+
+    <footer>
+        <p>&copy; <?php echo date("Y"); ?> Goto-Gro MRM</p>
+    </footer>
+</body>
+</html>
