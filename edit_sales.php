4e9b2c1f69831749bbeec382ffddd0d369a4ccc3
102619835 <115048372+102619835@users.noreply.github.com>
2023-09-22 19:56:46 +1000

    Update edit_sales.php

--- edit_sales.php
+++ edit_sales.php
@@ -90,7 +90,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 	<?php include 'header.php'; ?>
 	<?php include 'menu.php';   ?>
     <h1>Edit Sales Record</h1>
-    <form action="edit_sale.php" method="POST">
+    <form action="" method="POST">
         <label for="transaction_id">Transaction ID:</label>
         <input type="text" name="transaction_id" id="transaction_id" required>
         <br>
