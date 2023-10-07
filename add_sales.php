048ffff8aec95bae6ed215b1bda1cee676578db7
102619835 <115048372+102619835@users.noreply.github.com>
2023-10-07 18:53:17 +1100

    Update add_sales.php

--- add_sales.php
+++ add_sales.php
@@ -1,50 +1,72 @@
 <?php
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    include './io/databaseHandle.php';
 
-    if ($_SERVER["REQUEST_METHOD"] == "POST") {
-        include './io/databaseHandle.php';
-        
-        // Retrieve data from form submission
-        $product_name = $_POST["product_name"];
-        $quantity = $_POST["quantity"];
-        $price = $_POST["price"];
-             
-        $queryCommitInventory = "insert into mysql_schema.inventory values (DEFAULT, '$product_name', '$quantity', '$price', DEFAULT)";
-        $queryAttempt= $socket->execute_query($queryCommitInventory, null);
+    // Retrieve data from form submission
+    $username = $_POST["username"];
+    $price = $_POST["price"];
 
-        if (!$queryAttempt){
-            echo '<script>console.log("<debug> User commit failed"); </script>';
+
+    // Check if the username exists in the 'people' table
+    $queryCheckUsername = "SELECT COUNT(*) FROM mysql_schema.people WHERE username = ?";
+    
+    $stmtCheckUsername = $socket->prepare($queryCheckUsername);
+    $stmtCheckUsername->bind_param("s", $username);
+    $stmtCheckUsername->execute();
+    $stmtCheckUsername->bind_result($userExists);
+    $stmtCheckUsername->fetch();
+    $stmtCheckUsername->close();
+
+    if ($userExists) {
+        // Username exists, proceed with the transaction
+        // Serialize the PHP array (stockIDs) to store in the BLOB column
+        $stockIDs = []; // Replace this with your array data
+        $stockIDs_serialized = serialize($stockIDs);
+
+        // Assuming transactionID is an auto-increment column, you don't need to specify it
+        $queryCommitstoretransactions = "INSERT INTO mysql_schema.storetransactions VALUES (DEFAULT, ?, ?, '$price', DEFAULT)";
+        $stmt = $socket->prepare($queryCommitstoretransactions);
+
+        // Bind the username and serialized data
+        $stmt->bind_param("ss", $username, $stockIDs_serialized);
+
+        $queryAttempt = $stmt->execute();
+
+        if (!$queryAttempt) {
+            echo '<script>console.log("<debug> Transaction commit failed"); </script>';
         } else {
-            echo '<script>console.log("<debug> User commit succeeded"); </script>';
+            echo '<script>console.log("<debug> Transaction commit succeeded"); </script>';
         }
+        $stmt->close();
+    } else {
+        // Username does not exist, display an error message on the page
+        echo '<div style="color: red;">Username does not exist. Please check the username.</div>';
+    }
 
-        $socket->close();
-        }
+    $socket->close();
+}
 ?>
 
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <meta name="description" content="add_sales_record">
-    <title>Add Sales Record</title>
+    <meta name="description" content="store_transaction">
+    <title>Store Transaction</title>
     <link rel="stylesheet" type="text/css" href="styles.css">
 </head>
 <body>
-	<?php include 'header.php'; ?>
+    <?php include 'header.php'; ?>
     <?php include 'menu.php'; ?>
     <h1>Add Sales Record</h1>
-    <form action="" method="POST">
-        <label for="product_name">Product Name:</label>
-        <input type="text" name="product_name" id="product_name" required>
-        <br>
-        <label for="quantity">Quantity:</label>
-        <input type="number" name="quantity" id="quantity" required>
+    <form action="" method="POST" enctype="multipart/form-data">
+        <label for="username">Username:</label>
+        <input type="text" name="username" id="username" required>
         <br>
         <label for="price">Price per Unit:</label>
         <input type="number" name="price" id="price" required>
-        <br>
-        <input type="submit" value="Add Sale Record">
+        <input type="submit" value="Store Transaction">
     </form>
-	<?php include 'footer.php'; ?>
+    <?php include 'footer.php'; ?>
 </body>
 </html>
