4046de00dd407053682461ba9e29b35f16790ff5
102619835 <115048372+102619835@users.noreply.github.com>
2023-10-09 17:45:38 +1100

    Update add_sales.php

--- add_sales.php
+++ add_sales.php
@@ -24,6 +24,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
         // Calculate the total quantity of products and validate product names and quantities
         $totalQuantity = 0;
+        $validTransaction = true; // Flag to track if the transaction is valid
 
         foreach ($products as $key => $product) {
             $productName = $product["name"];
@@ -57,41 +58,46 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
                     $quantityExceededProducts[] = $productName;
                     // Close the statement for fetching the result
                     $stmtCheckProduct->close();
+                    $validTransaction = false; // Mark the transaction as invalid
                 }
             } else {
                 // Product does not exist in inventory, add it to the list of invalid products
                 $invalidProducts[] = $productName;
                 // Close the statement for fetching the result
                 $stmtCheckProduct->close();
+                $validTransaction = false; // Mark the transaction as invalid
             }
         }
 
-        // Display error messages for invalid products and quantity exceeded products
-        if (!empty($invalidProducts)) {
-            echo '<div style="color: red;">The following products are invalid and do not exist in inventory: ' . implode(", ", $invalidProducts) . '</div>';
-        }
-        if (!empty($quantityExceededProducts)) {
-            echo '<div style="color: red;">The following products have quantities that exceed current inventory: ' . implode(", ", $quantityExceededProducts) . '</div>';
-        }
+        // Check if the transaction is valid before inserting it into the database
+        if ($validTransaction) {
+            // Assuming transactionID is an auto-increment column, you don't need to specify it
+            $queryCommitstoretransactions = "INSERT INTO mysql_schema.storetransactions (username, stockIDs_serialized, txnSum) VALUES (?, ?, ?)";
+            $stmt = $socket->prepare($queryCommitstoretransactions);
 
-        // Assuming transactionID is an auto-increment column, you don't need to specify it
-        $queryCommitstoretransactions = "INSERT INTO mysql_schema.storetransactions (username, stockIDs_serialized, txnSum) VALUES (?, ?, ?)";
-        $stmt = $socket->prepare($queryCommitstoretransactions);
+            // Serialize an array of products for the 'stockIDs_serialized' column
+            $serializedProducts = serialize($products);
 
-        // Serialize an array of products for the 'stockIDs_serialized' column
-        $serializedProducts = serialize($products);
+            // Bind the username, serialized products, and total quantity as a float(10,2)
+            $stmt->bind_param("ssd", $username, $serializedProducts, $totalQuantity);
 
-        // Bind the username, serialized products, and total quantity as a float(10,2)
-        $stmt->bind_param("ssd", $username, $serializedProducts, $totalQuantity);
+            $queryAttempt = $stmt->execute();
 
-        $queryAttempt = $stmt->execute();
-
-        if (!$queryAttempt) {
-            echo '<script>console.log("<debug> Transaction commit failed"); </script>';
+            if (!$queryAttempt) {
+                echo '<script>console.log("<debug> Transaction commit failed"); </script>';
+            } else {
+                echo '<script>console.log("<debug> Transaction commit succeeded"); </script>';
+            }
+            $stmt->close();
         } else {
-            echo '<script>console.log("<debug> Transaction commit succeeded"); </script>';
+            // Display error messages for invalid products and quantity exceeded products
+            if (!empty($invalidProducts)) {
+                echo '<div style="color: red;">The following products are invalid and do not exist in inventory: ' . implode(", ", $invalidProducts) . '</div>';
+            }
+            if (!empty($quantityExceededProducts)) {
+                echo '<div style="color: red;">The following products have quantities that exceed current inventory: ' . implode(", ", $quantityExceededProducts) . '</div>';
+            }
         }
-        $stmt->close();
     } else {
         // Username does not exist, display an error message on the page
         echo '<div style="color: red;">Username does not exist. Please check the username.</div>';
@@ -122,15 +128,17 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
         <!-- Product input fields (can be dynamically added) -->
         <div id="products">
-            <div class="product">
-                <label for="product[0][name]">Product Name:</label>
-                <input type="text" name="product[0][name]" required>
-                <label for="product[0][quantity]">Quantity:</label>
-                <input type="number" name="product[0][quantity]" required>
-            </div>
-        </div>
-        <button type="button" onclick="addProduct()">Add Product</button>
-        <input type="submit" value="Add">
+    <div class="product">
+        <label for="product[0][name]">Product Name:</label>
+        <input type="text" name="product[0][name]" required>
+        <label for="product[0][quantity]">Quantity:</label>
+        <input type="number" name="product[0][quantity]" required>
+        <button type="button" onclick="deleteProduct(this)">Delete</button>
+    </div>
+</div>
+<button type="button" onclick="addProduct()">Add Product</button>
+<input type="submit" value="Add">
+
     </form>
     <?php
     include './io/databaseHandle.php';
@@ -140,37 +148,60 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $result = $socket->query($querySelectTransaction);
 
     if ($result) {
-        echo "<h2>Transaction Details:</h2>";
-        echo "<table>";
-        echo "<tr><th>TransactionID</th><th>Username</th><th>Product</th><th>Total Amount</th></tr>";
+    echo "<h2>Transaction Details:</h2>";
+    echo "<table>";
+    echo "<tr><th>TransactionID</th><th>Username</th><th>Product</th><th>Quantity</th><th>Price</th></tr>";
 
-        while ($row = $result->fetch_assoc()) {
-            echo "<tr>";
-            echo "<td>{$row['transactionID']}</td>";
-            echo "<td>{$row['username']}</td>";
+    while ($row = $result->fetch_assoc()) {
+        echo "<tr>";
+        echo "<td>{$row['transactionID']}</td>";
+        echo "<td>{$row['username']}</td>";
 
-            // Unserialize the product data
-            $stockDataSerialized = $row['stockIDs_serialized'];
-            $stockData = unserialize($stockDataSerialized);
+        // Unserialize the product data
+        $stockDataSerialized = $row['stockIDs_serialized'];
+        $stockData = unserialize($stockDataSerialized);
 
-            // Assuming $stockData is an array containing stock information
-            // Loop through the array and display the content
-            $productInfo = "";
-            foreach ($stockData as $stock) {
-                $productName = $stock['name'];
-                $quantity = $stock['quantity'];
-                $productInfo .= "$productName: $quantity<br>";
+        // Initialize variables to store product, quantity, and price information
+        $productInfo = "";
+        $quantityInfo = "";
+        $priceInfo = "";
+
+        foreach ($stockData as $stock) {
+            $productName = $stock['name'];
+            $quantity = $stock['quantity'];
+
+            // Fetch the unit price for the product from the inventory table
+            $queryUnitPrice = "SELECT unitPrice FROM mysql_schema.inventory WHERE productName = ?";
+            $stmtUnitPrice = $socket->prepare($queryUnitPrice);
+            $stmtUnitPrice->bind_param("s", $productName);
+            $stmtUnitPrice->execute();
+            $stmtUnitPrice->bind_result($unitPrice);
+
+            if ($stmtUnitPrice->fetch()) {
+                $price = $quantity * $unitPrice;
+                $priceInfo .= "$price<br>";
+            } else {
+                // Handle the case where the unit price couldn't be fetched
+                $priceInfo .= "N/A<br>";
             }
 
-            echo "<td>$productInfo</td>";
-            echo "<td>{$row['txnSum']}</td>";
-            echo "</tr>";
+            $productInfo .= "$productName<br>";
+            $quantityInfo .= "$quantity<br>";
+
+            // Close the statement for fetching the unit price
+            $stmtUnitPrice->close();
         }
 
-        echo "</table>";
-    } else {
-        echo "<p>No Transaction found.</p>";
+        echo "<td>$productInfo</td>";
+        echo "<td>$quantityInfo</td>";
+        echo "<td>$priceInfo</td>";
+        echo "</tr>";
     }
+
+    echo "</table>";
+} else {
+    echo "<p>No Transaction found.</p>";
+}
 
     // Close the database connection
     $socket->close();
@@ -178,23 +209,31 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
     <?php include 'footer.php'; ?>
 
-    <script>
-        // Function to add a new product input field
-        function addProduct() {
-            const productsDiv = document.getElementById('products');
-            const productCount = productsDiv.querySelectorAll('.product').length;
-            
-            const newProductDiv = document.createElement('div');
-            newProductDiv.className = 'product';
-            newProductDiv.innerHTML = `
-                <label for="product[${productCount}][name]">Product Name:</label>
-                <input type="text" name="product[${productCount}][name]" required>
-                <label for="product[${productCount}][quantity]">Quantity:</label>
-                <input type="number" name="product[${productCount}][quantity]" required>
-            `;
-            
-            productsDiv.appendChild(newProductDiv);
-        }
-    </script>
+<script>
+    // Function to add a new product input field
+    function addProduct() {
+        const productsDiv = document.getElementById('products');
+        const productCount = productsDiv.querySelectorAll('.product').length;
+        
+        const newProductDiv = document.createElement('div');
+        newProductDiv.className = 'product';
+        newProductDiv.innerHTML = `
+            <label for="product[${productCount}][name]">Product Name:</label>
+            <input type="text" name="product[${productCount}][name]" required>
+            <label for="product[${productCount}][quantity]">Quantity:</label>
+            <input type="number" name="product[${productCount}][quantity]" required>
+            <button type="button" onclick="deleteProduct(this)">Delete</button>
+        `;
+        
+        productsDiv.appendChild(newProductDiv);
+    }
+
+    // Function to delete a product input field
+    function deleteProduct(button) {
+        const productDiv = button.parentElement;
+        const productsDiv = document.getElementById('products');
+        productsDiv.removeChild(productDiv);
+    }
+</script>
 </body>
 </html>
