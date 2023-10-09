5c54e2257252f49dba03dd3ff6489133284df9af
SparshaPriom <145214425+SparshaPriom@users.noreply.github.com>
2023-10-09 22:18:23 +1100

    Update edit_sales.php

--- edit_sales.php
+++ edit_sales.php
@@ -1,82 +1,62 @@
 <?php
+// Database connection
+$serverURL = "gotogro-mrm-db.mysql.database.azure.com";
+$username = "mydemouser";
+$password = "Vsp3dbwH";
+$database = "mysql_schema";
+$certificate = './cert.pem';
+
+$socket = mysqli_init();
+mysqli_ssl_set($socket, NULL, NULL, $certificate, NULL, NULL);
+$connectionState = mysqli_real_connect($socket, $serverURL, $username, $password, $database, 3306, MYSQLI_CLIENT_SSL);
+
+if (!$connectionState) {
+    echo '<script>console.log("<debug> sqli connection failed"); </script>';
+    die('Failed to connect to MySQL: ' . mysqli_connect_error());
+} else {
+    echo '<script>console.log("<debug> sqli socket established"); </script>';
+}
+
+
+// Initialize variables
+$username = "";
+$editedSalesData = [];
+
+// Check if the form was submitted
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     // Retrieve data from the form
-    $transaction_id = $_POST["transaction_id"];
-    $product_name = $_POST["product_name"];
-    $quantity = $_POST["quantity"];
-    $price = $_POST["price"];
+    $username = $_POST["username"];
 
-    // Replace this with your database connection and query
-    $db_host = "gotogro-mrm-db.mysql.database.azure.com";
-    $db_username = "mydemouser";
-    $db_password = "Vsp3dbwH";
-    $db_name = "mysql_schema";
-	$certificate = 'cert/DigiCertGlobalRootCA.crt.pem';
+    if (isset($_POST["editSales"])) {
+        // Edit Sales Record
 
-    // Create a database connection
-    $conn = mysqli_init();
-    mysqli_ssl_set($conn, NULL, NULL, $certificate, NULL, NULL);
-    mysqli_real_connect($conn, $db_host, $db_username, $db_password, $db_name, 3306, MYSQLI_CLIENT_SSL);
+        // Retrieve edited sales record data from the form
+        $editedSalesData = $_POST["editedSalesData"];
 
-    // Check for connection errors
-    if ($conn->connect_error) {
-        die("Connection failed: " . $conn->connect_error);
-    }
+        // Check if the username exists in the 'people' table
+        $queryCheckUsername = "SELECT COUNT(*) FROM mysql_schema.people WHERE username = ?";
 
-    // Configure SSL options with the certificate file in the same directory
-	$mysqli->ssl_set(
-        $certificate,
-        null,
-        null,
-        null,
-        null
-    );
+        $stmtCheckUsername = $socket->prepare($queryCheckUsername);
+        $stmtCheckUsername->bind_param("s", $username);
+        $stmtCheckUsername->execute();
+        $stmtCheckUsername->bind_result($userExists);
+        $stmtCheckUsername->fetch();
+        $stmtCheckUsername->close();
 
-    // Establish the connection using SSL
-    if (!$conn->real_connect($db_host, $db_username, $db_password, $db_name)) {
-        die("Connection failed: " . $conn->connect_error);
-    }
+        if ($userExists) {
+            // Username exists, proceed with updating the sales record
+            // You can implement the code to update the sales record here
+            // For example, you can loop through the editedSalesData array and update the corresponding records in the database
 
-    // Check if the sale record exists
-    $check_sql = "SELECT * FROM sales WHERE transaction_id='$transaction_id'";
-    $result = $conn->query($check_sql);
+            // Make sure to handle any errors or validation as needed
 
-    if ($result->num_rows == 0) {
-        echo "Sale record with Transaction ID '$transaction_id' does not exist.";
-    } else {
-        // Update the sale record in the database
-        $update_sql = "UPDATE sales SET product_name='$product_name', quantity=$quantity, price=$price WHERE transaction_id='$transaction_id'";
-
-        if ($conn->query($update_sql) === TRUE) {
-            echo "Sale record updated successfully!";
+            // Display a success message if the update is successful
+            echo '<div style="color: green;">Sales record updated successfully</div>';
         } else {
-            echo "Error updating sale record: " . $conn->error;
+            // Username does not exist, display an error message on the page
+            echo '<div style="color: red;">Username does not exist. Please check the username.</div>';
         }
     }
-	
-	// Query to retrieve sales records
-    $sql = "SELECT * FROM sales";
-
-    // Execute the query
-    $result = $conn->query($sql);
-
-    if ($result->num_rows > 0) {
-        // Display table header
-        echo "<table border='1'>";
-        echo "<tr><th>Transaction ID</th><th>Product Name</th><th>Quantity</th><th>Price</th></tr>";
-
-        // Output data of each row
-        while ($row = $result->fetch_assoc()) {
-            echo "<tr><td>" . $row["transaction_id"] . "</td><td>" . $row["product_name"] . "</td><td>" . $row["quantity"] . "</td><td>" . $row["price"] . "</td></tr>";
-        }
-
-        echo "</table>";
-    } else {
-        echo "No sales records found.";
-    }
-
-    // Close the database connection
-    $conn->close();
 }
 ?>
 
@@ -84,31 +64,123 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <meta name="description" content="edit_sales_record">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Edit Sales Record</title>
-	<link rel="stylesheet" type="text/css" href="styles.css">
+    <link rel="stylesheet" type="text/css" href="styles.css">
 </head>
 <body>
-	<?php include 'header.php'; ?>
-	<?php include 'menu.php';   ?>
+    <?php include 'header.php'; ?>
+    <?php include 'menu.php'; ?>
     <h1>Edit Sales Record</h1>
-    <form action="" method="POST">
-        <label for="transaction_id">Transaction ID:</label>
-        <input type="text" name="transaction_id" id="transaction_id" required>
-        <br>
-        <label for="product_name">New Product Name:</label>
-        <input type="text" name="product_name" id="product_name" required>
-        <br>
-        <label for="quantity">New Quantity:</label>
-        <input type="number" name="quantity" id="quantity" required>
-        <br>
-        <label for="price">New Price per Unit:</label>
-        <input type="number" name="price" id="price" required>
-        <br>
-        <input type="submit" value="Edit Sale Record">
+    <form action="<?php echo $_SERVER['PHP_SELF']; ?>" method="POST">
+        <label for="username">Username:</label>
+        <input type="text" name="username" id="username" required>
+
+        <!-- Edit Sales Data -->
+        <h2>Edit Sales Data</h2>
+        <div id="editedSalesData">
+            <!-- You can dynamically generate input fields for editing sales data here -->
+            <!-- Example input fields -->
+            <div class="editedSalesItem">
+                <label for="product1">Product Name:</label>
+                <input type="text" name="editedSalesData[product1][name]" required>
+                <label for="quantity1">Quantity:</label>
+                <input type="number" name="editedSalesData[product1][quantity]" required>
+            </div>
+            <div class="editedSalesItem">
+                <label for="product2">Product Name:</label>
+                <input type="text" name="editedSalesData[product2][name]" required>
+                <label for="quantity2">Quantity:</label>
+                <input type="number" name="editedSalesData[product2][quantity]" required>
+            </div>
+        </div>
+
+        <button type="button" onclick="addEditedSalesItem()">Add Sales Item</button>
+
+        <input type="submit" name="editSales" value="Edit Sales Record">
     </form>
-	<?php
-    include 'footer.php';
+    <?php
+    // Retrieve and display member details (optional)
+    $querySelectTransaction = "SELECT * FROM mysql_schema.storetransactions";
+    $result = $socket->query($querySelectTransaction);
+
+    if ($result) {
+    echo "<h2>Transaction Details:</h2>";
+    echo "<table>";
+    echo "<tr><th>TransactionID</th><th>Username</th><th>Product</th><th>Quantity</th><th>Price</th></tr>";
+
+    while ($row = $result->fetch_assoc()) {
+        echo "<tr>";
+        echo "<td>{$row['transactionID']}</td>";
+        echo "<td>{$row['username']}</td>";
+
+        // Unserialize the product data
+        $stockDataSerialized = $row['stockIDs_serialized'];
+        $stockData = unserialize($stockDataSerialized);
+
+        // Initialize variables to store product, quantity, and price information
+        $productInfo = "";
+        $quantityInfo = "";
+        $priceInfo = "";
+
+        foreach ($stockData as $stock) {
+            $productName = $stock['name'];
+            $quantity = $stock['quantity'];
+
+            // Fetch the unit price for the product from the inventory table
+            $queryUnitPrice = "SELECT unitPrice FROM mysql_schema.inventory WHERE productName = ?";
+            $stmtUnitPrice = $socket->prepare($queryUnitPrice);
+            $stmtUnitPrice->bind_param("s", $productName);
+            $stmtUnitPrice->execute();
+            $stmtUnitPrice->bind_result($unitPrice);
+
+            if ($stmtUnitPrice->fetch()) {
+                $price = $quantity * $unitPrice;
+                $priceInfo .= "$price<br>";
+            } else {
+                // Handle the case where the unit price couldn't be fetched
+                $priceInfo .= "N/A<br>";
+            }
+
+            $productInfo .= "$productName<br>";
+            $quantityInfo .= "$quantity<br>";
+
+            // Close the statement for fetching the unit price
+            $stmtUnitPrice->close();
+        }
+
+        echo "<td>$productInfo</td>";
+        echo "<td>$quantityInfo</td>";
+        echo "<td>$priceInfo</td>";
+        echo "</tr>";
+    }
+
+    echo "</table>";
+} else {
+    echo "<p>No Transaction found.</p>";
+}
+
+    // Close the database connection
+    $socket->close();
     ?>
+
+
+    <?php include 'footer.php'; ?>
+
+    <script>
+        // Function to add a new edited sales item input fields
+        function addEditedSalesItem() {
+            const editedSalesDataDiv = document.getElementById('editedSalesData');
+            const newItemDiv = document.createElement('div');
+            newItemDiv.className = 'editedSalesItem';
+            newItemDiv.innerHTML = `
+                <label for="productName">Product Name:</label>
+                <input type="text" name="editedSalesData[productName][name]" required>
+                <label for="quantity">Quantity:</label>
+                <input type="number" name="editedSalesData[productName][quantity]" required>
+            `;
+            editedSalesDataDiv.appendChild(newItemDiv);
+        }
+    </script>
 </body>
 </html>
