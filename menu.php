ff18cb20b58db630ead3787ea236657b24d68d68
Siva <siva03g@gmail.com>
2023-10-13 23:02:03 +1100

    Added Correspondence

--- menu.php
+++ menu.php
@@ -5,8 +5,10 @@
         <li><a href="remove_member.php">Remove Member</a></li>
         <li><a href="add_sales.php">Add Sales</a></li>
         <li><a href="edit_sales.php">Edit Sales</a></li>
-        <li><a href="export_data.php">Export Sales/Member/Inventory Data</a></li>
+        <li><a href="export_data.php">Export to CSV</a></li>
         <li><a href="inventory.php">Inventory</a></li>
+        <li><a href="notification.php">Notification</a></li>
         <li><a href="optimisation.php">Order optimisation</a></li>
+        <li><a href="export_pdf.php">Receopt</a></li>
     </ul>
 </nav>
